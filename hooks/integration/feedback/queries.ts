ee7ad5abc7d1c44878809e320f6c30b3960c1220
Daniel Lopes <danieldeandradelopes@gmail.com>
2025-11-26 20:41:48 -0300

    feat: configure feedback get

---
+++ hooks/integration/feedback/queries.ts
@@ -0,0 +1,46 @@
+import { api } from "@/services/api";
+import { useQuery } from "@tanstack/react-query";
+import { FEEDBACK_KEYS } from "./keys";
+import Feedback from "@/src/@backend-types/Feedback";
+
+export const useGetFeedbacks = () => {
+  return useQuery<Feedback[], Error>({
+    queryKey: FEEDBACK_KEYS.list(),
+    queryFn: async () => {
+      const response = await api.get<Feedback[]>({
+        url: "/feedbacks",
+      });
+      return response;
+    },
+    retry: false,
+    refetchOnWindowFocus: true,
+  });
+};
+
+export const useGetFeedback = (id: number) => {
+  return useQuery<Feedback, Error>({
+    queryKey: FEEDBACK_KEYS.detail(id),
+    queryFn: async () => {
+      const response = await api.get<Feedback>({
+        url: `/feedbacks/${id}`,
+      });
+      return response;
+    },
+    retry: false,
+    enabled: !!id,
+  });
+};
+
+export const useGetFeedbacksByBox = (boxId: number) => {
+  return useQuery<Feedback[], Error>({
+    queryKey: FEEDBACK_KEYS.byBox(boxId),
+    queryFn: async () => {
+      const response = await api.get<Feedback[]>({
+        url: `/feedbacks/box/${boxId}`,
+      });
+      return response;
+    },
+    retry: false,
+    enabled: !!boxId,
+  });
+};
